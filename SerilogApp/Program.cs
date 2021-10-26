dd6876853aad517497ee18cdb03e5a98d976ce27
iamstan13y <iamstan13y@gmail.com>
2021-10-26 15:01:14 +0200

    Serilog NuGet references & configuration.

--- SerilogApp/Program.cs
+++ SerilogApp/Program.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Configuration;
+using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +15,14 @@ namespace SerilogApp
     {
         public static void Main(string[] args)
         {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+               
             CreateHostBuilder(args).Build().Run();
         }
 
